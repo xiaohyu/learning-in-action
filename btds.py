cb2cd9753cdd799ae4957f2cfdc165eab0273cc7
xiaohyu <hongruiyu@gmail.com>
2023-02-23 03:04:05 +0000

    just test

---
+++ btds.py
@@ -0,0 +1,15 @@
+import btds
+import importlib
+importlib.reload(btds)
+from btds import *
+
+def test():
+    """fdsaf d"""
+    print('foo')
+    return
+
+def test1():
+    """fdsaf dd"""
+    print('foo2')
+    return
+
